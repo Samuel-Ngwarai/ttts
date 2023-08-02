140b368f2a77cda38886d9e4252cc2bc8331a57f
Samuel Ngwarai <samuel.ngwarai@share-now.com>
2023-08-02 22:06:58 +0200

    add sentry cli to package.json

--- src/config/default.ts
+++ src/config/default.ts
@@ -6,7 +6,7 @@ const defaultConfig: IConfigObject = {
   CREATE_SWAGGER_FILE: false,
   CACHE_STD_TTL: 600,
   MAX_WAITING_TIME_FOR_PLAYER_2: 30,
-  SENTRY_DSN: '',
+  SENTRY_DSN: 'https://bc5abd91e87721d31d7f851fd330043c@o4505631328894976.ingest.sentry.io/4505637007392768',
   SENTRY_TRACES_SAMPLE_RATE: 1.0,
   ENVIRONMENT: 'development'
 };
